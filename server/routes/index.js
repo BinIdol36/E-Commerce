e3d83d03265e38b35fd6cfc0fdc68182188ad875
Hoang Phan <hoang1311176@gmail.com>
2023-05-17 11:22:57 +0700

    #3 hash password trong db và handle server error

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,7 +1,11 @@
 const userRouter = require('./userRouter')
+const { notFound, errHandler } = require('../middlewares/errHandler')
 
 const initRoutes = app => {
 	app.use('/api/user', userRouter)
+
+	app.use(notFound)
+	app.use(errHandler)
 }
 
 module.exports = initRoutes
