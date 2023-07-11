7e9eb3188cfc6f578f6c3b09a8dcdcb2803f9b94
Hoang Phan <hoang1311176@gmail.com>
2023-07-11 14:09:26 +0700

    #22 Header & Navigation

--- client/src/components/Sidebar.tsx
+++ client/src/components/Sidebar.tsx
@@ -1,6 +1,21 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
+import { apiGetCategories } from "../apis/app"
+import { NavLink } from "react-router-dom"
 
 function Sidebar() {
+  const [categories, setCategories] = useState(null)
+
+  const fetchCategories = async () => {
+    const response = await apiGetCategories()
+    if (response.success) setCategories(response.prodCategories)
+  }
+
+  useEffect(() => {
+    fetchCategories()
+  }, [])
+
+  console.log(categories)
+
   return <div>Sidebar</div>
 }
 
