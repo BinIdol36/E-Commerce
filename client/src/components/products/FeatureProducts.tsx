5bcc8d0ef28c6d4a84b60a022b1aa69ef6a0cf4d
Hoang Phan <hoang1311176@gmail.com>
2023-09-08 22:35:02 +0700

    #74 Display Grid

--- client/src/components/products/FeatureProducts.tsx
+++ client/src/components/products/FeatureProducts.tsx
@@ -8,7 +8,7 @@ const FeatureProducts = () => {
   const fetchProducts = async () => {
     const response = await apiGetProducts({
       limit: 9,
-      totalRating: 5,
+      sort: "-totalRating",
     })
 
     if (response.success) setProducts(response.products)
@@ -34,26 +34,26 @@ const FeatureProducts = () => {
           />
         ))}
       </div>
-      <div className="flex justify-between">
+      <div className="grid grid-cols-4 grid-rows-2 gap-4">
         <img
           src="https://digital-world-2.myshopify.com/cdn/shop/files/banner1-bottom-home2_b96bc752-67d4-45a5-ac32-49dc691b1958_600x.jpg?v=1613166661"
           alt=""
-          className="w-[50%] object-contain"
+          className="w-full h-full object-cover col-span-2 row-span-2"
         />
-        <div className="flex flex-col justify-between w-[24%]">
-          <img
-            src="https://digital-world-2.myshopify.com/cdn/shop/files/banner2-bottom-home2_400x.jpg?v=1613166661"
-            alt=""
-          />
-          <img
-            src="https://digital-world-2.myshopify.com/cdn/shop/files/banner3-bottom-home2_400x.jpg?v=1613166661"
-            alt=""
-          />
-        </div>
+        <img
+          src="https://digital-world-2.myshopify.com/cdn/shop/files/banner2-bottom-home2_400x.jpg?v=1613166661"
+          alt=""
+          className="w-full h-full object-cover col-span-1 row-span-1"
+        />
         <img
           src="https://digital-world-2.myshopify.com/cdn/shop/files/banner4-bottom-home2_92e12df0-500c-4897-882a-7d061bb417fd_400x.jpg?v=1613166661"
           alt=""
-          className="w-[24%] object-contain"
+          className="w-full h-full object-cover col-span-1 row-span-2"
+        />
+        <img
+          src="https://digital-world-2.myshopify.com/cdn/shop/files/banner3-bottom-home2_400x.jpg?v=1613166661"
+          alt=""
+          className="w-full h-full object-cover col-span-1 row-span-1"
         />
       </div>
     </div>
